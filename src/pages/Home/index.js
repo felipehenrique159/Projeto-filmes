f79d8817517498bd3190ebc8e213beb0396306cf
felipehenrique159 <felipecod6@hotmail.com>
2021-05-23 14:58:32 -0300

    Arquivos iniciais

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+import React ,{useState} from 'react'
+
+export default function Home(){
+    return(
+        <div>
+            <h1>Pagina Home</h1>
+        </div>
+
+    )
+}
