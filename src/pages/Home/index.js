145cfd0a9a2fe0acd44cffcffa6bffac25d4fa40
felipehenrique159 <felipecod6@hotmail.com>
2021-05-23 15:39:39 -0300

    instalação e config axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,27 @@
-// import {Link} from 'react-router-dom'
-// import React ,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 
-export default function Home(){
-    return(
+export default function Home() {
+
+    const [filmes, setFilmes] = useState([])
+
+    const url = 'r-api/?api=filmes/'
+
+   
+    useEffect(()=>{
+
+        async function getApiFilmes() {
+            const res = await api.get(url)
+            setFilmes(res.data)
+            console.log(res.data);
+        }
+
+        getApiFilmes()
+        
+     },[])
+
+    return (
         <div>
             <h1>Pagina Home</h1>
         </div>
